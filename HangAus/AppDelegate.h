33a9021f4f1c04e726da7fe5c379f921a73729a4
陈鑫荣 <chenxinrong@chenxinrongdemacbook-pro.local>
2018-04-09 11:16:55 +0800

    初始化

---
+++ HangAus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HangAus
+//
+//  Created by 陈鑫荣 on 2018/4/9.
+//  Copyright © 2018年 HangAus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
